23c30e9640d389c4f705e12dcddc305fce4cf879
DjAlok123 <69834328+DjAlok123@users.noreply.github.com>
2021-03-29 09:18:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,142 @@
+var database;
+
+var drawing = [];
+var currentPath = [];
+var isDrawing = false;
+
+function setup() {
+  canvas = createCanvas(500, 500);
+
+  canvas.mousePressed(startPath);
+  canvas.parent('canvascontainer');
+  canvas.mouseReleased(endPath);
+
+  var saveButton = select('#saveButton');
+  saveButton.mousePressed(saveDrawing);
+
+  var clearButton = select('#clearButton');
+  clearButton.mousePressed(clearDrawing);
+
+  // Your web app's Firebase configuration
+  var config = {
+    apiKey: "AIzaSyBno_oqUissy1W2NOYDnIFVM9JquOjvgeA",
+    authDomain: "c-35-a325e.firebaseapp.com",
+    databaseURL: "https://c-35-a325e-default-rtdb.firebaseio.com",
+    projectId: "c-35-a325e",
+    storageBucket: "c-35-a325e.appspot.com",
+    messagingSenderId: "711315374425",
+    appId: "1:711315374425:web:c592c010cab6e356a84c48"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+  database = firebase.database();
+
+
+  var params = getURLParams();
+  console.log(params);
+  if (params.id) {
+    console.log(params.id);
+    showDrawing(params.id);
+  }
+
+  var ref = database.ref('drawings');
+  ref.on('value', gotData, errData);
+}
+
+function startPath() {
+  isDrawing = true;
+  currentPath = [];
+  drawing.push(currentPath);
+}
+
+function endPath() {
+  isDrawing = false;
+}
+
+function draw() {
+  background(0);
+
+  if (isDrawing) {
+    var point = {
+      x: mouseX,
+      y: mouseY
+    };
+    currentPath.push(point);
+  }
+
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  for (var i = 0; i < drawing.length; i++) {
+    var path = drawing[i];
+    beginShape();
+    for (var j = 0; j < path.length; j++) {
+      vertex(path[j].x, path[j].y);
+    }
+    endShape();
+  }
+}
+
+function saveDrawing() {
+  var ref = database.ref('drawings');
+  var data = {
+    name: 'Dan',
+    drawing: drawing
+  };
+  var result = ref.push(data, dataSent);
+  console.log(result.key);
+
+  function dataSent(err, status) {
+    console.log(status);
+  }
+}
+
+function gotData(data) {
+  // clear the listing
+  var elts = selectAll('.listing');
+  for (var i = 0; i < elts.length; i++) {
+    elts[i].remove();
+  }
+
+  var drawings = data.val();
+  var keys = Object.keys(drawings);
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    //console.log(key);
+    var li = createElement('li', '');
+    li.class('listing');
+    var ahref = createA('#', key);
+    ahref.mousePressed(showDrawing);
+    ahref.parent(li);
+
+    var perma = createA('?id=' + key, 'permalink');
+    perma.parent(li);
+    perma.style('padding', '4px');
+
+    li.parent('drawinglist');
+  }
+}
+
+function errData(err) {
+  console.log(err);
+}
+
+function showDrawing(key) {
+  //console.log(arguments);
+  if (key instanceof MouseEvent) {
+    key = this.html();
+  }
+
+  var ref = database.ref('drawings/' + key);
+  ref.once('value', oneDrawing, errData);
+
+  function oneDrawing(data) {
+    var dbdrawing = data.val();
+    drawing = dbdrawing.drawing;
+    //console.log(drawing);
+  }
+}
+
+function clearDrawing() {
+  drawing = [];
+}
